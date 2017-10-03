03f381f36aaf643be899ec129a0d6a364a328287
kr28mevada <kr28mevada@gmail.com>
2017-10-03 11:47:49 +0530

    Latest changes commited on 28th Sep after circleci file changes_Appium_24

--- app/src/main/java/com/circle_ci/MainActivity.java
+++ app/src/main/java/com/circle_ci/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//sdf
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
