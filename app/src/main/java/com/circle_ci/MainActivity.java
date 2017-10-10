72340962175577a42027e9416d039cac209887d8
kr28mevada <kr28mevada@gmail.com>
2017-10-10 16:41:18 +0530

    Added hocky , Firbase and crashlytics

--- app/src/main/java/com/circle_ci/MainActivity.java
+++ app/src/main/java/com/circle_ci/MainActivity.java
@@ -5,16 +5,28 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.circle_ci.fragments.LoginFragment;
+import com.crashlytics.android.Crashlytics;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import net.hockeyapp.android.CrashManager;
 import net.hockeyapp.android.UpdateManager;
 
-public class MainActivity extends Activity {
+import io.fabric.sdk.android.Fabric;
 
+public class MainActivity extends Activity {
+    private FirebaseAnalytics mFirebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Fabric.with(this, new Crashlytics());
+
+        Bundle params = new Bundle();
+        params.putString("action", "launch");
+        mFirebaseAnalytics.logEvent("Init", params);
 
         getFragmentManager().beginTransaction().add(R.id.frameLayout, new LoginFragment())
                 .addToBackStack(LoginFragment.class.getSimpleName())
--- app/src/main/java/com/circle_ci/fragments/HomeFragment.java
+++ app/src/main/java/com/circle_ci/fragments/HomeFragment.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.circle_ci.R;
+import com.crashlytics.android.Crashlytics;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class HomeFragment extends Fragment {
 
@@ -17,7 +19,38 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        rootView.findViewById(R.id.btnEvent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    Bundle params = new Bundle();
+                    params.putString("Button", "Clicked");
+                    FirebaseAnalytics.getInstance(getActivity()).logEvent("action", params);
+
+                    Crashlytics.log("button_click");
+
+                    String s1=null;
+                    s1.length();
+
+
+
+                }
+                catch (Exception e)
+                {
+                    Crashlytics.logException(e);
+                }
+                String s=null;
+                s.length();
+
+
+            }
+        });
+
+        Crashlytics.log("after click");
+
+        return rootView;
     }
 
 }
