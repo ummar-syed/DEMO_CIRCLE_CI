fee166152b6b632cd806ebd138cccb43a4efde2f
kr28mevada <kr28mevada@gmail.com>
2017-10-16 11:19:10 +0530

    Latest code commited

--- app/src/main/java/com/circle_ci/MainActivity.java
+++ app/src/main/java/com/circle_ci/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends Activity {
                 .addToBackStack(LoginFragment.class.getSimpleName())
                 .commit();
         checkForUpdates();
+        //
     }
 
     @Override
