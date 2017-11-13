34a2963a04e985f4756209d92ca474df51d5d557
kr28mevada <32267196+kr28mevada@users.noreply.github.com>
2017-11-13 16:38:22 +0530

    Update DeployArtifact.sh

--- DeployArtifact.sh
+++ DeployArtifact.sh
@@ -1,2 +1,2 @@
-curl -u akhil_rj:e9dcb689-0ea0-489e-8e85-c93de2ce7f65 -X POST -H "Content-Type: application/octet-stream" https://saucelabs.com/rest/v1/storage/Kr28Mevada/Newupload.apk?overwrite=true --data-binary @app/build/outputs/apk/app-debug.apk
+curl -u akhil_rj:e9dcb689-0ea0-489e-8e85-c93de2ce7f65 -X POST -H "Content-Type: application/octet-stream" https://saucelabs.com/rest/v1/storage/akhil_rj/Newupload.apk?overwrite=true --data-binary @app/build/outputs/apk/app-debug.apk
 echo "Copied"
