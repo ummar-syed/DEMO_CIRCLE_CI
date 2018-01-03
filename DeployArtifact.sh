d41af3c99829a1b32aba162efc149dbcdf95a2c0
ummar-syed <32639379+ummar-syed@users.noreply.github.com>
2018-01-03 14:37:06 +0530

    Update DeployArtifact.sh

--- DeployArtifact.sh
+++ DeployArtifact.sh
@@ -1,2 +1,1 @@
-curl -u akhil_rj:e9dcb689-0ea0-489e-8e85-c93de2ce7f65 -X POST -H "Content-Type: application/octet-stream" https://saucelabs.com/rest/v1/storage/akhil_rj/Newupload.apk?overwrite=true --data-binary @app/build/outputs/apk/app-debug.apk
-echo "Copied"
+curl -u ummar-syed:6b0427f5-1ee4-47ed-91cb-abb991ddc9bb -X POST -H "Content-Type: application/octet-stream" https://saucelabs.com/rest/v1/storage/ummar-syed/Newupload.apk?overwrite=true --data-binary @app/build/outputs/apk/app-debug.apk
